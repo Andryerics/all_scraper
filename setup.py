5a3ae0166ba42bb4d32a8628eb5ace5f2d677a01
Andryerics <137793417+Andryerics@users.noreply.github.com>
2024-09-06 11:06:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,27 @@
-# setup.py
+import setuptools
+from pinscrape._version import __version__
 
-
-from setuptools import setup, find_packages
-
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
-    name='all_scraper',
-    version='1.0.0',
-    packages=find_packages(),
-    install_requires=[
-        'requests',
-    ],
-    include_package_data=True,
-    description='Scrape all social network such as : LinkedIn, Tiktok, Instagram.....',
+with open("requirements.txt", "r") as req:
+    reqs = req.read().split("\n")
+
+setuptools.setup(
+    name="all_scraper",
+    version=__version__,
+    author="Andryerics",
+    author_email="andryerics@gmail.com",
+    description="Scraper all social network such as : LinkedIn, Tiktok, Instagram, Facebook.....",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Andryerics/all_scraper',
-    author='Andry RL',
-    author_email='andryerics@gmail.com',
-    license='MIT',
-    keywords = ['Scraping', 'Scraper', 'linkedin','instagram','tiktok'],
+    url="https://github.com/Andryerics/all_scraper",
+    packages=setuptools.find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=reqs,
 )
