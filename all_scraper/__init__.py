237a030e1e4f7f85117376cd9c21a1e64290c28b
Andryerics <137793417+Andryerics@users.noreply.github.com>
2024-09-06 12:20:00 +0300

    Update __init__.py

--- all_scraper/__init__.py
+++ all_scraper/__init__.py
@@ -1,1 +1,9 @@
 from .scraper import Scraper
+from ._version import __version__ as _version
+
+__version__ = _version
+__author__ = "Andry RL"
+__copyright__ = "Copyright © 2023-2024"
+__license__ = "MIT"
+__maintainer__ = "Andry RL"
+__email__ = "andryerics@gmail.com"
