0204c3f34bdd6a3e8d4533225e86f85cb7b6d325
Andryerics <137793417+Andryerics@users.noreply.github.com>
2024-09-06 13:23:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="all_scraper",  # Le nom du package à installer via pip
-    version="1.0.0",
-    description="Scraper all social network such as : LinkedIn, Tiktok, Instagram, Facebook.....",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="Andryerics",
-    author_email="andryerics@gmail.com",
-    url="https://github.com/Andryerics/all_scraper",  # Le lien vers ton repo GitHub
-    packages=find_packages(),  # Trouve automatiquement tous les sous-modules (facebook_api, insta_api, etc.)
-    install_requires=[
-        "httpx",
-        "loguru",
-        "httpx[http2]",
-        "parsel",
-        "lxml"
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setup()
