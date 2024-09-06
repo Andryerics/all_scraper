4f45ce5785b0f5a0ddb0a909504c125b48ee3b68
Andryerics <137793417+Andryerics@users.noreply.github.com>
2024-09-06 13:16:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-import setuptools
-from pinscrape._version import __version__
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r") as req:
-    reqs = req.read().split("\n")
-
-setuptools.setup(
-    name="all_scraper",
-    version=__version__,
+setup(
+    name="all_scraper",  # Le nom du package à installer via pip
+    version="1.0.0",
+    description="Scraper all social network such as : LinkedIn, Tiktok, Instagram, Facebook.....",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Andryerics",
     author_email="andryerics@gmail.com",
-    description="Scraper all social network such as : LinkedIn, Tiktok, Instagram, Facebook.....",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Andryerics/all_scraper",
-    packages=setuptools.find_packages(),
+    url="https://github.com/Andryerics/all_scraper",  # Le lien vers ton repo GitHub
+    packages=find_packages(),  # Trouve automatiquement tous les sous-modules (facebook_api, insta_api, etc.)
+    install_requires=[
+        "httpx",
+        "loguru",
+        "httpx[http2]",
+        "parsel",
+        "lxml"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=reqs,
 )
